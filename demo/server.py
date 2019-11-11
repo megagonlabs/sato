afaf33d2164e0ad9a7939a01c9106db069bf33ea
Dan <dzhang@cs.umass.edu>
2019-11-11 10:57:38 -0500

    update demo

--- demo/server.py
+++ demo/server.py
@@ -13,13 +13,13 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+app.config['MAX_CONTENT_LENGTH'] =  5 * 1024 * 1024
 
 
-# a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
 
+@app.route('/')
+def upload():
+    return render_template('upload.html')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -32,7 +32,7 @@ def upload_file():
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return render_template('upload.html')
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
@@ -55,9 +55,7 @@ def upload_file():
     </form>
     '''
 
-@app.route('/')
-def upload():
-    return render_template('upload.html')
+
 
 
 def extract_feature(df):
@@ -67,29 +65,46 @@ def extract_feature(df):
 def predict():
 
 
-    filename = request.form.get('fId')
     
+    filename = request.form.get('fId')
     df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
     res = evaluate(df)
 
-    return render_template('table.html',  tables=[df.to_html(classes='data', max_rows=30, index=False, header=True)],
-                            prediction=res,
+    return render_template('table.html', 
+                            tables=[df.to_html(classes="table table-striped table-bordered",
+                                             max_rows=30, 
+                                             index=False,
+                                             header=True, 
+                                             border=False)],
+                            prediction='Sato prediction: ' + str(res),
                             showButton=False)
+
+
 
 
 @app.route('/uploads/<filename>')
 def process_file(filename):
     df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    
+    if df.shape[0]>200 or df.shape[1] > 6:
+        return render_template('error.html', 
+                             cols = df.shape[1],
+                             rows = df.shape[0])
+
     return render_template('table.html', 
-                            tables=[df.to_html(classes='data', max_rows=30, index=False, header=True)],
+                            tables=[df.to_html(classes="table table-striped table-bordered",
+                                             max_rows=30, 
+                                             index=False,
+                                             header=True, 
+                                             border=False)],
+                            prediction='',
                             filename=filename,
                             showButton=True)
 
 
 
 
+
+
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0')
