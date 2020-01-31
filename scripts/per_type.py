6eb0174b344cf3bebca057863c83400e2f50b33b
Dan Zhang <dzhang@cs.umass.edu>
2020-01-31 16:40:46 -0500

    script fix

--- scripts/per_type.py
+++ scripts/per_type.py
@@ -1,8 +1,8 @@
-from analysis import data_gen_all
+from analysis_functions import data_gen_all
 import os
 basepath = os.environ['BASEPATH']
 
 path = os.path.join(basepath, 'results/CRF_log/type78/CRF_path')
 pathL = os.path.join(basepath, 'results/CRF_log/type78/CRF+LDA_pathL')
 
-data_gen_all(path, path_L, 'multi-col', './output')
+data_gen_all(path, pathL, 'multi-col', './output')
